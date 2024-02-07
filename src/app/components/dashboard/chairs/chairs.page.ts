fc9d9b562ee50eab7ec879fcccee45ea9888c6fe
Gary Yaral <gary.yaral@gmail.com>
2024-02-06 19:36:14 -0500

    Second commit

--- src/app/components/dashboard/chairs/chairs.page.ts
+++ src/app/components/dashboard/chairs/chairs.page.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-chairs',
   templateUrl: './chairs.page.html',
   styleUrls: ['./chairs.page.scss'],
 })
-export class ChairsPage implements OnInit {
-
+export class ChairsPage {
   constructor() { }
+  chair: FormGroup = new FormGroup({
+    type: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+  })
 
-  ngOnInit() {
+  public alertButtons = [
+    {
+      text: 'No',
+      cssClass: 'alert-button-cancel',
+    },
+    {
+      text: 'Yes',
+      cssClass: 'alert-button-confirm',
+      handler: () => {
+        this.saveChair()
+      }
+    },
+  ];
+
+
+  @ViewChild(IonModal) modal!: IonModal;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
 
+  confirm() {
+
+  }
+
+  onWillDismiss(event: any) {
+    const modal = event.target
+  }
+
+  saveChair() {
+    console.log('dentro');
+
+  }
+
+  detectChange($event: any, name: string) {
+    const value = ($event.target as HTMLInputElement).value
+    let cleanText = value.replace(/\s/g, '')
+    const isEmpty = cleanText === ''
+
+
+  }
 }
--- src/app/components/dashboard/dashboard.page.ts
+++ src/app/components/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { RestApiService } from 'src/app/services/rest-api.service';
 import { WINDOW_TITLES } from 'src/constants';
 
@@ -23,7 +24,7 @@ export class DashboardPage implements OnInit {
     { title: 'Reservaciones', url: '/dashboard/reservations', icon: 'qr-code' },
     { title: 'Paquetes', url: '/dashboard/packages', icon: 'cube' },
     { title: 'Pagos', url: '/dashboard/payments', icon: 'card' },
-    { title: 'Configuración', url: '/dashboard/settings', icon: 'options' },
+    { title: 'Configuración', url: '/dashboard/settings', icon: 'options' }
   ];
   public labels = [
     { title: 'Web', url: '/dashboard/spam', icon: 'globe' }
@@ -31,14 +32,22 @@ export class DashboardPage implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private restApi: RestApiService
+    private restApi: RestApiService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
-    const viewParam = this.route.snapshot.params['view'];
-    if(viewParam && viewParam !== '') {
-      this.view = WINDOW_TITLES[viewParam]
-    }
+    // Cargamos el titulo de la sección
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      if(!event) {return}
+      if(!event.url) {return}
+      if(typeof event.url !== 'string') {return}
+      if(!event.url.includes('/')){return}
+      const path = event.url.split('/')
+      this.view = WINDOW_TITLES[path[path.length -1]]
+    });
     this.userLogged = 'Federico Pilares Montiel'
   }
 
